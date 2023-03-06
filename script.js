7c03bdc3729671be09e427992b2b18aaf3edb687
Sam <samuelpulukuri@outlook.com>
2023-03-07 00:43:48 +0530

    Complete adding computers choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function getComputersChoice() {
+  const choice = Math.floor(Math.random() * 3);
+
+  switch (choice) {
+    case 0:
+      return "ROCK";
+
+    case 1:
+      return "PAPER";
+
+    case 2:
+      return "SCISSORS";
+  }
+}
+console.log(getComputersChoice());
